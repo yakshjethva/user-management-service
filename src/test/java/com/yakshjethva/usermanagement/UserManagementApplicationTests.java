53fe1ea7aaadd8e7fc12d405a1e3790bd5dc8388
Yaksh <ctrlaltdel@myyahoo.com>
2025-02-17 12:39:33 -0500

    spring init

---
+++ src/test/java/com/yakshjethva/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakshjethva.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
