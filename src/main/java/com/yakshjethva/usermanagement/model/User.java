caa72d359108b91224629a8240ad71578885642f
Yaksh <ctrlaltdel@myyahoo.com>
2025-02-18 21:24:49 -0500

    [SCRUM-6] - Implement UserService (business logic for registration & profile updates)

--- src/main/java/com/yakshjethva/usermanagement/model/User.java
+++ src/main/java/com/yakshjethva/usermanagement/model/User.java
@@ -1,4 +1,4 @@
-package model;
+package com.yakshjethva.usermanagement.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
--- src/main/java/model/Role.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Role {
-	USER, ADMIN
-}
